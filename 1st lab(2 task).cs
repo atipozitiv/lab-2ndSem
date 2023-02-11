96df05c7ea65094549e4673b33c40a913c566596
atipozitiv <117564743+atipozitiv@users.noreply.github.com>
2023-02-11 21:30:09 +0700

    Create 1st lab(2 task).cs

---
+++ 1st lab(2 task).cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace lab_1
+{
+	internal class Program
+	{
+		static void Main(string[] args) {
+			Console.WriteLine("введите число");
+			String FirstNumber = Console.ReadLine();
+			int Length = FirstNumber.Length;
+			String SecondNumber = "";
+      Char SecondDigit = FirstNumber[1];
+      for (int Position = 0; Position < Length; ++Position) {
+				if (Position != 1) {
+					SecondNumber += FirstNumber[Position];
+				}
+			}
+			SecondNumber += SecondDigit;
+			Console.WriteLine(SecondNumber);
+			Console.ReadKey();
+		}
+	}
+}
